2f86003357e26b87029f31aa542b147df6cadf79
pinedav <jpinedav@alumnos.unex.es>
2021-03-17 17:44:12 +0100

    as

--- ComponentSM/src/specificworker.py
+++ ComponentSM/src/specificworker.py
@@ -25,6 +25,10 @@ from genericworker import *
 import cv2
 import numpy as np
 import time
+import matplotlib.pyplot as plt
+from pytransform3d import rotations as pr
+from pytransform3d import transformations as pt
+from pytransform3d.transform_manager import TransformManager
 
 # If RoboComp was compiled with Python bindings you can use InnerModel in Python
 sys.path.append('/opt/robocomp/lib')
@@ -78,10 +82,17 @@ class SpecificWorker(GenericWorker):
     def setParams(self, params):
         self.params = params
         self.viewimage = "true" in self.params["viewimage"]
+
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111, projection='3d')
+        plt.show()
+
         return True
 
     @QtCore.Slot()
     def compute(self):
+
+        self.prueba()
 
         if self.new_image:
             self.count += 1
@@ -112,6 +123,30 @@ class SpecificWorker(GenericWorker):
             self.total = self.count
             self.count = 0
             self.begin = time.time()
+
+
+
+    def prueba(self):
+        object2cam = pt.transform_from(
+            pr.active_matrix_from_intrinsic_euler_xyz(np.array([1.25, -0.028, -0.06])),
+            np.array([381, 761, -3173]))
+
+        object2cam2 = pt.transform_from(
+            pr.active_matrix_from_intrinsic_euler_xyz(np.array([-2.04, -3.13, 0.09])),
+            np.array([-192, 254, -3408]))
+
+        tm = TransformManager()
+        tm.add_transform("object", "camera", object2cam)
+        tm.add_transform("object", "camera2", object2cam2)
+
+        p = pt.transform(tm.get_transform("camera", "object"), np.array([0, 0, 0, 1]))
+
+        ax = tm.plot_frames_in("object", s=500)
+        ax.scatter(p[0], p[1], p[2])
+        ax.set_xlim((-1000, 3000))
+        ax.set_ylim((-3000, 3000))
+        ax.set_zlim((0, 4000))
+        plt.draw()
 
     def transform_to_world(self, people):
         for person in people.peoplelist:
