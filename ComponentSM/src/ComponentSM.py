d6966cd118f804dfe34950318d83fb20243e3443
pbustos <pbustos@unex.es>
2021-03-17 16:34:05 +0100

    working

--- ComponentSM/src/ComponentSM.py
+++ ComponentSM/src/ComponentSM.py
@@ -62,11 +62,11 @@ import time
 import os
 import copy
 import argparse
-from termcolor import colored
 # Ctrl+c handling
 import signal
 
 from PySide2 import QtCore
+from PySide2 import QtWidgets
 
 from specificworker import *
 
@@ -99,7 +99,7 @@ def sigint_handler(*args):
     QtCore.QCoreApplication.quit()
     
 if __name__ == '__main__':
-    app = QtCore.QCoreApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     parser = argparse.ArgumentParser()
     parser.add_argument('iceconfigfile', nargs='?', type=str, default='etc/config')
     parser.add_argument('--startup-check', action='store_true')
--- ComponentSM/src/specificworker.py
+++ ComponentSM/src/specificworker.py
@@ -24,6 +24,7 @@ from PySide2.QtWidgets import QApplication
 from genericworker import *
 import cv2
 import numpy as np
+import time
 
 # If RoboComp was compiled with Python bindings you can use InnerModel in Python
 sys.path.append('/opt/robocomp/lib')
@@ -60,6 +61,11 @@ class SpecificWorker(GenericWorker):
         self.xMano = 0
         self.yMano = 0
         self.Period = 50
+        self.count = 0
+        self.total = 0
+        self.begin = time.time()
+        self.cameras = {}
+
         if startup_check:
             self.startup_check()
         else:
@@ -74,25 +80,92 @@ class SpecificWorker(GenericWorker):
         self.viewimage = "true" in self.params["viewimage"]
         return True
 
+    # @QtCore.Slot()
+    # def compute(self):
+    #     #print('SpecificWorker.compute...')
+    #     #self.Almac_Personas()
+    #     #print(self.contPersonas)
+    #     #self.Gesto1()
+    #     #for Num in range(self.contPersonas):
+    #         #print(self.peopleAux.peoplelist[Num].joints['right_wrist'].y)
+    #         #print(self.peopleAux.peoplelist[Num].joints['left_wrist'].y)
+    #
+    #     if self.new_image:
+    #       self.imgCV = np.frombuffer(self.imgCruda.image, np.uint8).reshape(self.imgCruda.height, self.imgCruda.width, self.imgCruda.depth)
+    #       self.new_image = False
+    #     if self.new_people:
+    #         self.ProcesImg(self.people, self.imgCV)
+    #         cv2.imshow("Camera " + str(self.imgCruda.cameraID), self.imgCV)
+    #         cv2.waitKey(1)
+    #     return True
 
     @QtCore.Slot()
     def compute(self):
-        #print('SpecificWorker.compute...')
-        #self.Almac_Personas()
-        #print(self.contPersonas)
-        #self.Gesto1()
-        #for Num in range(self.contPersonas):
-            #print(self.peopleAux.peoplelist[Num].joints['right_wrist'].y)
-            #print(self.peopleAux.peoplelist[Num].joints['left_wrist'].y)
+        self.count += 1
 
         if self.new_image:
-          self.imgCV = np.frombuffer(self.imgCruda.image, np.uint8).reshape(self.imgCruda.height, self.imgCruda.width, self.imgCruda.depth)
-          self.new_image = False
-        if self.new_people:
-            self.ProcesImg(self.people, self.imgCV)
-            cv2.imshow("Camera " + str(self.imgCruda.cameraID), self.imgCV)
-            cv2.waitKey(1)
-        return True
+            self.cameras[self.imgCruda.cameraID] = np.frombuffer(self.imgCruda.image, np.uint8).reshape(
+                self.imgCruda.height, self.imgCruda.width, self.imgCruda.depth)
+            cv2.putText(self.cameras[self.imgCruda.cameraID], str(self.total) + ' fps', (10, 450),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)
+            cv2.putText(self.cameras[self.imgCruda.cameraID], str(len(self.people.peoplelist)) + ' bodies', (500, 450),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
+            self.new_image = False
+
+            if self.new_people:
+                #self.transform_to_world(self.people)
+                self.cameras[self.imgCruda.cameraID] = self.draw_body(self.people, self.cameras[self.imgCruda.cameraID])
+
+            self.cameras[self.imgCruda.cameraID] = cv2.cvtColor(self.cameras[self.imgCruda.cameraID], cv2.COLOR_BGR2RGB)
+            pix = QPixmap.fromImage(
+                QImage(self.cameras[self.imgCruda.cameraID], self.cameras[self.imgCruda.cameraID].shape[1],
+                       self.cameras[self.imgCruda.cameraID].shape[0], QImage.Format_RGB888))
+            if self.imgCruda.cameraID == 1:
+                self.ui.label_img_left.setPixmap(pix)
+            if self.imgCruda.cameraID == 3:
+                self.ui.label_img_right.setPixmap(pix)
+
+        if time.time() - self.begin > 1:
+            self.total = self.count
+            self.count = 0
+            self.begin = time.time()
+
+    def transform_to_world(self, people):
+        for person in people.peoplelist:
+            for name1, name2 in SKELETON_CONNECTIONS:
+                try:
+                    joint1 = person.joints[name1]
+                    joint2 = person.joints[name2]
+                    if joint1.score > 0.5 and joint2.score > 0.5:
+                        p = pt.transform(self.tm.get_transform("camera_1", "object"),
+                                         np.array([joint1.x, joint1.y, joint1.z, 1]))
+
+                        ax = self.tm.plot_frames_in("object", s=500)
+                        ax.scatter(p[0], p[1], p[2])
+                        ax.set_xlim((-1000, 3000))
+                        ax.set_ylim((-3000, 3000))
+                        ax.set_zlim((-2000.0, 4000))
+                        plt.show()
+                except:
+                    pass
+
+        # Draw body parts on image
+
+    def draw_body(self, people, image):
+        for person in people.peoplelist:
+            for name1, name2 in SKELETON_CONNECTIONS:
+                try:
+                    joint1 = person.joints[name1]
+                    joint2 = person.joints[name2]
+                    if joint1.score > 0.5:
+                        cv2.circle(image, (joint1.i, joint1.j), 10, (0, 0, 255))
+                    if joint2.score > 0.5:
+                        cv2.circle(image, (joint2.i, joint2.j), 10, (0, 0, 255))
+                    if joint1.score > 0.5 and joint2.score > 0.5:
+                        cv2.line(image, (joint1.i, joint1.j), (joint2.i, joint2.j), (0, 255, 0), 2)
+                except:
+                    pass
+        return image
 
     def Almac_Personas(self):
         self.contPersonas = len(self.peopleAux.peoplelist)
